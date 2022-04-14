6d8996083432a98fe506cc08807967303528a68c
boryantheone <boryantheone228@yandex.ru>
2022-04-14 23:44:40 +0300

    end 5 laba

--- 5 laba/flckr.js
+++ 5 laba/flckr.js
@@ -1,17 +1,45 @@
+var slideshow = function(tag) {
+	// console.log(tag);
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + 
+			"tags=" + tag + "&format=json&jsoncallback=?";
+	var displayMessage = function (messageIndex) {
+		if (tag !== "")  {
+			$.getJSON(url, function (flickrResponse) {
+				console.log(messageIndex);
+				var $img = $("<img>").attr("src", flickrResponse.items[messageIndex].media.m).hide();
+				$("main .photos").empty();
+				$("main .photos").append($img);
+				$img.fadeIn();
+				setTimeout(function () {
+					messageIndex = messageIndex + 1;
+					displayMessage(messageIndex);
+				}, 3000);
+				// if (messageIndex === 2) {
+				// 		messageIndex = -1;
+				// }
+			});
+		}
+	};
+	displayMessage(0);
+}
+
 var main = function () {
-	"use strict";
-	// на самом деле это всего одна строка
-	var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=cats&format=json&jsoncallback=?";
-	$.getJSON(url, function (flickrResponse) {
-		flickrResponse.items.forEach(function (photo) {
-			// создаем новый элемент jQuery для хранения изображений, но пока скрываем его
-			var $img = $("<img>").hide();
-			// устанавливаем атрибут для URL, находящегося в ответе
-			$img.attr("src", photo.media.m);
-			// прикрепляем тег к функции main элемента photos, а затем отображаем его
-			$("main .photos",).append($img);
-			$img.fadeIn();
-		});
+	"use strict"; 
+	var tag = "";
+	var $inputLabel = $("<p>").text("Введите тег для темы слайд-шоу: "),
+		$input = $("<input>").addClass("tag"), 				
+		$button = $("<button>").text("Поиск");
+	$button.on("click", function () {
+		var tag = "";
+		tag = $input.val();
+		console.log(tag);
+		$input.val("");
+		if (tag !== "") {
+			$("main .photos").empty();
+			slideshow(tag);
+		}
 	});
+	$("main .content").append($inputLabel).append($input).append($button); 
+	// console.log(tag);
 };
 $(document).ready(main);
